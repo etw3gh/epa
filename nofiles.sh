8a397ef3bd432d19568c6ad554db452c1e4979df
Eli Tabello <etabw3@gmail.com>
2016-12-18 08:06:09 +0000

    nofiles.sh strip header line

--- nofiles.sh
+++ nofiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat www.epa.gov.csv | awk {'print $1'} | tr "," " "  | grep -v ".pdf" | grep -v ".zip" | grep -v ".docx" | grep -v  ".xls" | grep -v ".txt" | grep -v ".gz" | uniq | sort > cleaned_nofiles.txt
+cat www.epa.gov.csv | tail -n+2 | awk {'print $1'} | tr "," " "  | grep -v ".pdf" | grep -v ".zip" | grep -v ".docx" | grep -v  ".xls" | grep -v ".txt" | grep -v ".gz" | uniq | sort > cleaned_nofiles.txt
 
