e6a19a4bd045a9963e21dfbeefd7ff04838fefd8
Eli Tabello <etabw3@gmail.com>
2016-12-18 07:51:10 +0000

    sort and remove dupes in nofiles.sh

--- nofiles.sh
+++ nofiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat www.epa.gov.csv | awk {'print $1'} | tr "," " "  | grep -v ".pdf" | grep -v ".zip" | grep -v ".docx" | grep -v  ".xls" | grep -v ".txt" | grep -v ".gz" > cleaned_nofiles.txt
+cat www.epa.gov.csv | awk {'print $1'} | tr "," " "  | grep -v ".pdf" | grep -v ".zip" | grep -v ".docx" | grep -v  ".xls" | grep -v ".txt" | grep -v ".gz" | uniq | sort > cleaned_nofiles.txt
 
